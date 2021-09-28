84e1bf0798c01f829ccda3324189984d99b2c47c
Çayan K <30707106+cayank@users.noreply.github.com>
2021-09-29 00:33:45 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,26 +1,26 @@
-// Hareket animasyonu gerçekleşecek -- Movement animation to happen
+// Movement animation to happen
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 
-// Öğeler -- Items
+// Items
 const title = document.querySelector(".title");
 const sneaker = document.querySelector(".sneaker img");
 const purchase = document.querySelector(".purchase");
 const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
 
-// Hareketli animasyon etkinliği -- Moving animation event
+// Moving animation event
 container.addEventListener("mousemove", (e) => {
   let x = (window.innerWidth / 2 - e.pageX) / 25;
   let y = (window.innerHeight / 2 - e.pageY) / 25;
   card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
 });
 
-// Animasyon İçinde -- Animation in
+// Animation in
 container.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
 
-  //Pencere -- Popout
+  // Popout
   title.style.transform = "translateZ(150px)";
   sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
   description.style.transform = "translateZ(125px)";
@@ -28,12 +28,12 @@ container.addEventListener("mouseenter", (e) => {
   purchase.style.transform = "translateZ(75px)";
 });
 
-// Animasyon Dışında -- Animate out
+// Animate out
 container.addEventListener("mouseleave", (e) => {
   card.style.transition = "all 0.5s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 
-  // Geri -- Popback
+  // Popback
   title.style.transform = "translateZ(0px)";
   sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
   description.style.transform = "translateZ(0px)";
